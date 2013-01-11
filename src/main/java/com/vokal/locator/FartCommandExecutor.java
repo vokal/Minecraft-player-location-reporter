2692f778ce503bad83b34f01e8007a48eb0d5638
Scott Ferguson <scottwferg@gmail.com>
2013-01-10 22:30:52 -0600

    Initial commit

---
+++ src/main/java/com/vokal/locator/FartCommandExecutor.java
@@ -0,0 +1,28 @@
+package com.vokal.locator;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+public class FartCommandExecutor implements CommandExecutor {
+    private PlayerLocator mPlugin; // pointer to your main class, unrequired if you don't need methods from the main class
+ 
+	public FartCommandExecutor(PlayerLocator aPlugin) {
+		mPlugin = aPlugin;
+	}
+ 
+	@Override
+	public boolean onCommand(CommandSender aSender, Command aCmd, String aLabel, String[] aArgs) {
+        if (!(aSender instanceof Player)) {
+            aSender.sendMessage("This command can only be run by a player.");
+            return false;
+        }
+
+        Player player = (Player) aSender;
+        mPlugin.getServer().broadcastMessage(ChatColor.DARK_RED + player.getDisplayName() + " farted!");
+
+        return true;
+	}
+}
---
+++ src/main/java/com/vokal/locator/PlayerLocator.java
@@ -0,0 +1,57 @@
+package com.vokal.locator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerLocator extends JavaPlugin {
+    private Runnable mUpdateLocations = new Runnable() {
+        @Override 
+        public void run() {
+            Player[] players = getServer().getOnlinePlayers();
+            
+            final JSONArray location_list = new JSONArray();
+            for (Player player : players) {
+                JSONObject loc = new JSONObject();
+                loc.put("player", player.getDisplayName());
+                loc.put("x", player.getLocation().getX());
+                loc.put("y", player.getLocation().getY());
+                loc.put("z", player.getLocation().getZ());
+                
+                location_list.add(loc);
+            }
+            
+            new Thread() {
+                public void run() {
+                    try {
+                        HttpClient client = new DefaultHttpClient();
+                        HttpPost httpPost = new HttpPost("http://localhost:5000/update");
+                        httpPost.setEntity(new StringEntity(location_list.toString()));
+                        httpPost.setHeader("Accept", "application/json");
+                        httpPost.setHeader("Content-type", "application/json; charset=UTF-8");
+                        
+                        client.execute(httpPost);
+                    } catch (Exception e) {
+                        // Log
+                        getLogger().warning(e.toString());
+                    }
+                }
+            }.start();
+        }
+    };
+
+	@Override
+	public void onEnable() {
+        getCommand("gob").setExecutor(new GobCommandExecutor(this));
+        getCommand("fart").setExecutor(new FartCommandExecutor(this));
+        
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, mUpdateLocations, 600L, 600L);
+	}
+}
