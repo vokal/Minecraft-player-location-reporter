957a8890a8c051115f8f0c17bef4bd82de7c8c6f
Scott Ferguson <scott.ferguson@vokalinteractive.com>
2013-01-14 12:40:02 -0600

    Add listener for players logging in to report to server

---
+++ src/main/java/com/vokal/locator/event/PlayerLoginListener.java
@@ -0,0 +1,24 @@
+package com.vokal.locator.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.entity.Player;
+
+import com.vokal.locator.PlayerLocator;
+
+public class PlayerLoginListener implements Listener {
+    private PlayerLocator mPlugin; // pointer to your main class, unrequired if you don't need methods from the main class
+
+    public PlayerLoginListener(PlayerLocator aPlugin) {
+        mPlugin = aPlugin;
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent aEvent) {
+        Player[] players = new Player[mPlugin.getServer().getOnlinePlayers().length + 1];
+        players[players.length - 1] = aEvent.getPlayer();
+
+        mPlugin.updateLocations(players);
+    }
+}
